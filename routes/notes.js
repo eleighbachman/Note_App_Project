73879402c1e773a0da2bd084e21a838b9d35599b
Tyler Foss <tylerscottfoss@gmail.com>
2017-02-09 19:19:55 -0700

    routes updated

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Note = require('../models/note');
 
-/*GET animals page */
+/*GET notes page */
 router.get('/', function(req, res) {
   Note.find( function(err, notes) {
     res.render('notes', {notes: notes })
@@ -24,6 +24,29 @@ router.get('/:id', function(req, res) {
   Note.findById(req.params.id, function(err, note) {
     res.render('note', {note: note});
   })
+})
+
+router.put('/:id', function(req, res) {
+    var body = {
+      title: req.body.title,
+      description: req.body.description,
+      updatedAt: Date.now()
+    }
+
+
+  Note.findByIdAndUpdate(req.params.id, { $set: body }, { new: true }, function(err, note) {
+    res.redirect('/notes/' + req.params.id)
+  })
+});
+
+
+
+router.delete('/:id', function(req, res) {
+Note.findById(req.params.id, function(err, note) {
+  note.remove( function(err, note) {
+    res.redirect('/notes');
+  });
+});
 })
 
 module.exports = router;
