bb8eca20a443aca7726e42cf7c6711173add9443
Liz Bachman <eleighbachman@gmail.com>
2017-02-09 18:36:24 -0700

    Fixed the local host

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/animals');
+mongoose.connect('mongodb://localhost/notes');
 
 var index = require('./routes/index');
 
